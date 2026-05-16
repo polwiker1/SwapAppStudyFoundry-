c7959d3fb5d1171ad506d1e3112bc0747938958c
polwiker1 <cervezawicca@hotmail.com.ar>
2026-05-16 10:04:38 -0300

    Fix formatting for getAmountsOut signature

--- test/SwapApp.t.sol
+++ test/SwapApp.t.sol
@@ -28,7 +28,11 @@ contract MockRouter {
         amounts[path.length - 1] = amountIn;
     }
 
-    function getAmountsOut(uint256 amountIn, address[] calldata path) external pure returns (uint256[] memory amounts) {
+    function getAmountsOut(uint256 amountIn, address[] calldata path)
+        external
+        pure
+        returns (uint256[] memory amounts)
+    {
         amounts = new uint256[](path.length);
         amounts[0] = amountIn;
         amounts[path.length - 1] = amountIn;
